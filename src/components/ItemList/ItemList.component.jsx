7c778ea0690c3b458d3685b16e864ddf38d4e258
Niya <disma1589@gmail.com>
2022-07-12 19:06:15 +0200

    added items context

--- src/components/ItemList/ItemList.component.jsx
+++ src/components/ItemList/ItemList.component.jsx
@@ -1,23 +1,28 @@
-export function ItemList(props) {
-    const itemElements = props.items.map((item) => (
-      <div key={item.id}>
-        <input type="checkbox"
-          checked={item.isDone}
-          onChange={props.onMarkAsDone(item)}
-        />
-        <span>{item.text} ({item.displayCreatedAt()})</span>
-        {item.isDone &&
-          <span>&mdash; Completed at: {item.displayCompletedAt()}</span>
-        }
-        <button type="button" onClick={props.onDelete(item)}>
-          Delete
-        </button>
-      </div>
-    ));
-  
-    return (
-      <div>
-        {itemElements}
-      </div>
-    );
-  }
+import { useContext } from "react";
+import { ItemsContext } from "../../contexts/ItemsContext";
+
+export function ItemList() {
+  const context = useContext(ItemsContext);
+
+  const itemElements = context.items.map((item) => (
+    <div key={item.id}>
+      <input type="checkbox"
+        checked={item.isDone}
+        onChange={context.markItemAsDone(item)}
+      />
+      <span>{item.text} ({item.displayCreatedAt()})</span>
+      {item.isDone &&
+        <span>&mdash; Completed at: {item.displayCompletedAt()}</span>
+      }
+      <button type="button" onClick={context.deleteItem(item)}>
+        Delete
+      </button>
+    </div>
+  ));
+
+  return (
+    <div>
+      {itemElements}
+    </div>
+  );
+}
