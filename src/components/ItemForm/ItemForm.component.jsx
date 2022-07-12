2a7246e0d32d28d7bc0657bcdedb01dc9f3feda2
Niya <disma1589@gmail.com>
2022-07-12 20:50:43 +0200

    improved filtering UI and fixed form bugs

--- src/components/ItemForm/ItemForm.component.jsx
+++ src/components/ItemForm/ItemForm.component.jsx
@@ -1,10 +1,11 @@
 export function ItemForm(props) {
-    return (
-      <form onSubmit={props.onSubmit}>
-        <input type="text" placeholder="TODO: Buy milk"
-          name="item" value={props.value} onChange={props.onChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={props.onSubmit} autoComplete="off">
+      <input type="text" placeholder="TODO: Buy milk"
+        name="item" value={props.value} onChange={props.onChange}
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
